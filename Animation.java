c38d26c5ac0a09a505bd9971433aefc9f6ef5cf2
Jared Lyon <jaredalyon@gmail.com>
2022-10-06 15:32:04 -0400

    submit

--- Animation.java
+++ Animation.java
@@ -52,7 +52,7 @@ class Dot {
    * 
    * methods for fields: none
    */
-  
+
   // draw this Dot as CircleImage with its size and color
   WorldImage draw() {
     return new CircleImage(this.radius, "solid", this.c);
@@ -72,7 +72,7 @@ class Dot {
 
 //represents a list of Dots
 interface ILoDot {
-  
+
   /* TEMPLATE
    * 
    * fields: none
@@ -85,7 +85,7 @@ interface ILoDot {
    * 
    * methods for fields: none
    */
-  
+
   // draws Dots from this list onto the given scene
   WorldScene draw(WorldScene acc);
 
@@ -111,7 +111,7 @@ class MtLoDot implements ILoDot {
    * 
    * methods for fields: none
    */
-  
+
   // draws Dots from this empty list onto the accumulated
   // image of the scene so far
   public WorldScene draw(WorldScene acc) {
@@ -138,7 +138,7 @@ class ConsLoDot implements ILoDot {
     this.first = first;
     this.rest = rest;
   }
-  
+
   /* TEMPLATE
    * 
    * fields: none
@@ -183,7 +183,7 @@ class DotsWorld extends World {
   DotsWorld(ILoDot dots) {
     this.dots = dots;
   }
-  
+
   /* TEMPLATE
    * 
    * fields:
@@ -216,6 +216,12 @@ class DotsWorld extends World {
     return new DotsWorld(add.move());
   }
 
+  // test method for onTick using seeded randomness
+  public World onTickTesting() {
+    ILoDot add = new ConsLoDot(new Dot(10, Color.magenta, 1), this.dots);
+    return new DotsWorld(add.move());
+  }
+
   // add a key event to change the colors of all of the existing Dots in this
   // World to green when the "g" key is pressed
   public World onKeyEvent(String key) {
@@ -236,6 +242,9 @@ class ExamplesAnimation {
   Dot d1 = new Dot(10, Color.magenta, 2);
   Dot d2 = new Dot(10, Color.magenta, 5);
   Dot d3 = new Dot(10, Color.magenta, 10);
+  Dot d1Moved = new Dot(10, Color.magenta, 513, 175);
+  Dot d2Moved = new Dot(10, Color.magenta, 292, 95);
+  Dot d3Moved = new Dot(10, Color.magenta, 518, 383);
   Dot d4 = new Dot(10, Color.magenta, 10, 10);
   Dot d5 = new Dot(10, Color.magenta, 15, 20);
   Dot d6 = new Dot(10, Color.magenta, 15, 13);
@@ -248,20 +257,14 @@ class ExamplesAnimation {
   Dot d12 = new Dot(10, Color.green, 15, 20);
   Dot d13 = new Dot(10, Color.green, 15, 13);
   Dot d14 = new Dot(10, Color.green, 20, 23);
-  
-  Dot dm4 = new Dot(10, Color.magenta, 4);
-  Dot dm3 = new Dot(10, Color.magenta, this.d3.x + 5, this.d3.y + 3);
-  Dot dm2 = new Dot(10, Color.magenta, this.d2.x + 5, this.d2.y + 3);
-  Dot dm1 = new Dot(10, Color.magenta, this.d1.x + 5, this.d1.y + 3);
-  
-  ILoDot lodm1 =  new ConsLoDot(this.dm3,
-                     new ConsLoDot(this.dm2,
-                         new ConsLoDot(this.dm1, new MtLoDot())));
 
   ILoDot mt = new MtLoDot();
   ILoDot lod1 = new ConsLoDot(this.d1, this.mt);
   ILoDot lod2 = new ConsLoDot(this.d2, this.lod1);
   ILoDot lod3 = new ConsLoDot(this.d3, this.lod2);
+  ILoDot lod3Moved = new ConsLoDot(this.d3Moved,
+      new ConsLoDot(this.d2Moved,
+          new ConsLoDot(this.d1Moved, this.mt)));
   ILoDot lod4 = new ConsLoDot(this.d5, new ConsLoDot(this.d4, this.mt));
   ILoDot lod5 = new ConsLoDot(this.d7, new ConsLoDot(this.d6, this.mt));
 
@@ -302,8 +305,7 @@ class ExamplesAnimation {
   boolean testMove(Tester t) {
     return t.checkExpect(this.d4.move(), this.d6) 
         && t.checkExpect(this.d5.move(), this.d7)
-        && t.checkExpect(this.lod3.move(), this.lodm1)
-        && t.checkExpect(this.lod4.move(), this.lod5);
+        && t.checkExpect(this.lod3.move(), this.lod3Moved);
   }
 
   // test setGreen method
@@ -328,9 +330,14 @@ class ExamplesAnimation {
 
   // test onTick method
   boolean testOnTick(Tester t) {
-    return t.checkExpect(this.dw1.onTick(), new DotsWorld((new ConsLoDot(new Dot(10, Color.magenta, 10), this.dw1.dots)).move()));
+    return t.checkExpect(this.dw1.onTickTesting(),
+        new DotsWorld(new ConsLoDot(new Dot(10, Color.magenta, 1), this.dw1.dots).move()))
+        && t.checkExpect(this.dw2.onTickTesting(),
+            new DotsWorld(new ConsLoDot(new Dot(10, Color.magenta, 1), this.dw2.dots).move()))
+        && t.checkExpect(this.dw3.onTickTesting(),
+            new DotsWorld(new ConsLoDot(new Dot(10, Color.magenta, 1), this.dw3.dots).move()));
   }
-  
+
   // test onKeyEvent method
   boolean testOnKeyEvent(Tester t) {
     return t.checkExpect(this.dw1.onKeyEvent("g"), this.dw6)
