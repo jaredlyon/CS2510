c4cd98e9cef72bb9ad42fa7a5e8ab5f1eb2aee47
Jared Lyon <jaredalyon@gmail.com>
2022-10-19 10:20:12 -0400

    add A6

---
+++ FeedingFrenzy.java
@@ -0,0 +1,163 @@
+import tester.*;
+import javalib.worldimages.*;
+import javalib.funworld.*;
+import java.awt.Color;
+import java.util.Random;
+
+class Player {
+  int speed;
+  int size;
+  Color color;
+  int x;
+  int y;
+  
+  // generate the player
+  Player(int speed, Color color) {
+    this.speed = speed;
+    this.size = 10;
+    this.color = color;
+    this.x = 300; // middle of screen
+    this.y = 200; // middle of screen
+  }
+  
+  // constructor for moving the player
+  Player(int speed, int size, Color color, int x, int y) {
+    this.speed = speed;
+    this.size = size;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+  
+  // draw this Dot as CircleImage with its size and color
+  WorldImage draw() {
+    return new CircleImage(this.size, "solid", this.color);
+  }
+  
+  // move the player up
+  Player moveUp() {
+    return new Player(this.speed, this.size, this.color, this.x, this.y + this.speed);
+  }
+  
+  // move the player down
+  Player moveDown() {
+    return new Player(this.speed, this.size, this.color, this.x, this.y - this.speed);
+  }
+  
+  // move the player right
+  Player moveRight() {
+    return new Player(this.speed, this.size, this.color, this.x + this.speed, this.y);
+  }
+  
+  // move the player left
+  Player moveLeft() {
+    return new Player(this.speed, this.size, this.color, this.x - this.speed, this.y);
+  }
+  
+  // kill the player
+  
+  // grow the player
+  Player grow() {
+    return new Player(this.speed, this.size + 1, this.color, this.x, this.y);
+  }
+}
+
+class Enemy {
+  int speed;
+  int size;
+  Color color;
+  int x;
+  int y;
+  
+  // randomly generate an enemy
+  Enemy(int speed, int size, Color color) {
+    this.speed = speed;
+    this.color = color;
+    Random rand = new Random(); // randomly places the enemy on screen
+    this.x = rand.nextInt(600);
+    this.y = rand.nextInt(400);
+    this.size = rand.nextInt(25); // gives this enemy a random size
+  }
+  
+  // constructor for moving an enemy
+  Enemy(int speed, int size, Color color, int x, int y) {
+    this.speed = speed;
+    this.size = size;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+  
+  // draw this fish as CircleImage with its size and color
+  WorldImage draw() {
+    return new CircleImage(this.size, "solid", this.color);
+  }
+  
+  // moves the enemy
+  Enemy move() {
+    return new Enemy(this.speed, this.size, this.color, this.x + this.speed, this.y);
+  }
+  
+  // kills the enemy ?
+}
+
+interface IList<T> {
+  // draws fish from this list onto the given scene
+  WorldScene draw(WorldScene acc);
+  
+  // moves this list of Fish
+  IList<T> move();
+}
+
+class MtList<T> implements IList<T> {
+  
+  MtList() {
+    
+  }
+
+  @Override
+  public WorldScene draw(WorldScene acc) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public IList<T> move() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
+
+class ConsList<T> implements IList<T> {
+  T first;
+  IList<T> rest;
+  
+  ConsList(T first, IList<T> rest) {
+    this.first = first;
+    this.rest = rest;
+  }
+
+  @Override
+  public WorldScene draw(WorldScene acc) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public IList<T> move() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
+
+class FishWorld extends World {
+  
+}
+
+class ExamplesGame {
+  ExamplesGame() {
+    
+  }
+}
