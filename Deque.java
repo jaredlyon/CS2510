109f2cffde92efeec2a5c2df263f2f00be8f32ab
liamobuachalla <114438472+liamobuachalla@users.noreply.github.com>
2022-11-08 14:07:18 -0500

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -2,10 +2,21 @@ import java.util.function.Predicate;
 
 import tester.Tester;
 
+
+
 abstract class ANode<T> {
   ANode<T> next;
   ANode<T> prev;
   
+  
+  void connectNext(ANode<T> node) {};                            // Why are these both empty?
+  
+  void connectPrev(ANode<T> node) {};
+  
+  // finds the node that passed the predicate
+  ANode<T> findNode(Predicate<T> pred) {
+    return this;
+  }
 }
 
 
@@ -49,8 +60,8 @@ class Node<T> extends ANode<T> {
   
   
   // finds node that passes the predicate
-  Node<T> findNode(Predicate<T> pred) {
-    if (pred.apply(this)) {
+  ANode<T> findNode(Predicate<T> pred) {
+    if (pred.test(this)) {
       return this;
     }
     else {
@@ -88,13 +99,8 @@ class Sentinel<T> extends ANode<T>{
   
   // removes node from the tail of the deque
   void removeTail() {
-    this.prev = this.prev.prev;              // this is really ugly code, it needs a helper to declutter
+    this.prev = this.prev.prev;               // this is really ugly code, it needs a helper to declutter
     this.prev.prev.next = this;
-  }
-  
-  // finds the node that passed the predicate
-  Node<T> findNode(Predicate<T> pred) {                         // if the findNode method ends back up at sentinel, give exception
-    return new RuntimeException("No such node exists");
   }
 
 }
@@ -117,35 +123,31 @@ class Deque<T> {
     return 0;
   }
   
-  // CHANGE THE NEXT AND PREV OF EACH TO SKIP OR ADD NEW NODE
   
-
-  
-  
-  // adds a node to the head of the deque
+  // EFFECT: adds a node to the head of the deque
   void addAtHead(T data) {
     Node<T> node = new Node<T>(data);
     this.header.insertNext(node);
   }
   
-  // adds a node to the tail of the deque
+  // EFFECT: adds a node to the tail of the deque
   void addAtTail(T data) {
     Node<T> node = new Node<T>(data);
     this.header.insertPrev(node);
   }
   
-  // removes a node from the head of the deque
+  // EFFECT: removes a node from the head of the deque
   void removeFromHead() {
     this.header.removeHead();
   }
   
-  // removes a node from the tail of the deque.
+  // EFFECT: removes a node from the tail of the deque.
   void removeFromTail() {
     this.header.removeTail();
   }
   
   // finds the node that passed the given predicate
-  Node<T> find(Predicate<T> pred) {
+  ANode<T> find(Predicate<T> pred) {
     return this.header.next.findNode(pred);                     // field of a field
   }
   
@@ -157,23 +159,118 @@ class Deque<T> {
 
 class ExamplesDeque {
   
-  ANode<String> sent;
+  Deque<String> dequealpha;
+  Deque<String> dequelength;
+  
+  Sentinel<String> sentalpha;
+  Sentinel<String> sentlength;
+  
+  ANode<String> zab;
   ANode<String> abc;
   ANode<String> bcd;
   ANode<String> cde;
   ANode<String> def;
+  ANode<String> efg;
+  
+  ANode<String> hi;
+  ANode<String> dog;
+  ANode<String> four;
+  ANode<String> apple;
+  ANode<String> banana;
+  ANode<String> welcome;
+
   
   void initData() {
-    this.sent = new Sentinel<String>();
+    
+    
+    this.sentalpha = new Sentinel<String>();
     this.abc = new Node<String>("abc");
     this.bcd = new Node<String>("bcd");
     this.cde = new Node<String>("cde");
     this.def = new Node<String>("def");
     
-    this.sent.next = this.abc;
-    this.sent.prev = this.def;
+    this.zab = new Node<String>("zab");
+    this.efg = new Node<String>("efg");
+    
+    this.sentalpha.next = this.abc;
+    this.sentalpha.prev = this.def;
     this.def.prev = this.cde;
+    this.cde.next = this.def;
     this.cde.prev = this.bcd;
+    this.bcd.next = this.cde;
     this.bcd.prev = this.abc;
+    this.abc.next = this.bcd;
+    
+    this.dequealpha = new Deque<String>(this.sentalpha);
+    
+    
+    this.sentlength = new Sentinel<String>();
+    this.dog = new Node<String>("dog");
+    this.four = new Node<String>("four");
+    this.apple = new Node<String>("apple");
+    this.banana = new Node<String>("banana");
+    
+    this.hi = new Node<String>("hi");
+    this.welcome = new Node<String>("welcome");
+    
+    this.sentlength.next = this.dog;
+    this.sentlength.prev = this.banana;
+    this.banana.prev = this.apple;
+    this.apple.next = this.banana;
+    this.apple.prev = this.four;
+    this.four.next = this.apple;
+    this.four.prev = this.dog;
+    this.dog.next = this.four;
+    
+    this.dequelength = new Deque<String>(this.sentlength);
   }
+  
+  // tests for addAtHead method
+  void testAddAtHead(Tester t) {                                    // EXPECTED NODE NEEDS TO UPDATE
+    this.initData();
+    t.checkExpect(this.sentalpha.next, this.abc);
+    this.dequealpha.addAtHead("zab");
+    t.checkExpect(this.sentalpha.next, this.zab);
+    
+    t.checkExpect(this.sentlength.next, this.dog);
+    this.dequelength.addAtHead("hi");
+    t.checkExpect(this.sentlength.next, this.hi);
+  }
+  
+  // tests for addAtTail method
+  void testAddAtTail(Tester t) {
+    this.initData();
+    t.checkExpect(this.sentalpha.prev, this.def);
+    this.dequealpha.addAtTail("efg");
+    t.checkExpect(this.sentalpha.prev, this.efg);
+    
+    t.checkExpect(this.sentlength.prev, this.banana);
+    this.dequelength.addAtTail("welcome");
+    t.checkExpect(this.sentlength.prev, this.welcome);
+  }
+  
+  // tests for removeFromTail method
+  void testRemoveFromHead(Tester t) {
+    this.initData();
+    t.checkExpect(this.sentalpha.next, this.abc);
+    this.dequealpha.removeFromHead();
+    t.checkExpect(this.sentalpha.next, this.bcd);
+    
+    t.checkExpect(this.sentlength.next, this.dog);
+    this.dequelength.removeFromHead();
+    t.checkExpect(this.sentlength.next, this.four);
+  }
+  
+  // tests for removeFromTail method
+  void testRemoveFromTail(Tester t) {
+    this.initData();
+    t.checkExpect(this.sentalpha.prev, this.def);
+    this.dequealpha.removeFromTail();
+    t.checkExpect(this.sentalpha.prev, this.cde);
+    
+    t.checkExpect(this.sentlength.prev, this.banana);
+    this.dequelength.removeFromTail();
+    t.checkExpect(this.sentlength.prev, this.apple);
+  }
+  
 }
