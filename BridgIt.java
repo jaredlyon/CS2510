966bac398d454f651faea1944bc4822c90d5cb86
liamobuachalla <114438472+liamobuachalla@users.noreply.github.com>
2022-11-30 15:19:18 -0500

    Update BridgIt.java

--- BridgIt.java
+++ BridgIt.java
@@ -4,14 +4,18 @@ import javalib.impworld.*;
 import java.awt.Color;
 import javalib.worldimages.*;
 
-class Piece {
-  Color color;
-  Piece up;
-  Piece down;
-  Piece left;
-  Piece right;
+interface ANode {
   
-  Piece(Color color, Piece up, Piece down, Piece left, Piece right) {
+}
+
+class Node implements ANode {
+  Color color;
+  Node up;
+  Node down;
+  Node left;
+  Node right;
+  
+  Node(Color color, Node up, Node down, Node left, Node right) {
     this.color = color;
     this.up = up;
     this.down = down;
@@ -19,7 +23,7 @@ class Piece {
     this.right = right;
   }
   
-  Piece(Color color) {
+  Node(Color color) {
     this.color = color;
     this.up = null;
     this.down = null;
@@ -28,7 +32,15 @@ class Piece {
   }
 }
 
-// create an empty piece for edge pieces
+class Empty implements ANode {   // represents a node at a boundary
+  Color color;
+  
+  Empty(Color color) {           // create an empty piece for edge pieces
+    this.color = Color.WHITE;
+  }
+}
+
+
 
 //represents a fifteen game using tiles
 class BridgIt extends World {
@@ -39,12 +51,23 @@ class BridgIt extends World {
   BridgIt(int size) {
     if(this.size <= 3 || this.size % 2 == 0) {
       this.size = size + 1;
+    }
       else {
         this.size = size;
       }
     }
-  }
 
+  // create in new method, put in constructor
+  // the creation of all the pieces belongs in the game constructor, depends on gameboard size (in indices)
+  for (i = 0; i <= this.size; i = i + 2) {                   // increment by two, alternates nodes and empty nodes
+    for (j = 1; j <= this.size; j = j + 2) {
+      // Node temp = new Node(Color, empty, empty, empty, empty)
+      // next node -> new Node(Color, empty, empty, list.get(i - 1)., empty)
+    }
+  }
+  
+  
+  
 @Override
 public WorldScene makeScene() {
   // TODO Auto-generated method stub
